81f8bc63425375f5b478ff922f246d5b9f5a8c66
Sikandar897 <sikandarafridi6@gmail.com>
2024-02-14 20:02:22 +0500

    add:goomap

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyAe-vKTAN7T4BPKUoTelGNh2kCKooU3QEo")
+    GMSServices.provideAPIKey("$APi_Key")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
